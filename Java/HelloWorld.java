3f6b69304b91fbf58ed11e0f86246826b907ba3a
ivan <blorg@blorg.com>
2023-03-23 15:28:08 -0700

    First Java Program
    Hello World in Java

---
+++ Java/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class HelloWorld {
+  
+  
+  public static void main(String[] args) { 
+    
+    System.out.println("Hello World");
+
+    System.out.print("Java" + " is " + "fun");
+    
+  }
+ 
+  
+  /* ADD YOUR CODE HERE */
+  
+}
